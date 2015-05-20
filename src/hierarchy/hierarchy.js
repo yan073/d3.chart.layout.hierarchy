723c4baabe6912cf1a8052c68569afaa0e3715ef
Anna Bansaghi <anna.bansaghi@gmail.com>
2015-05-20 14:03:39 +0200

    Add colors feature

--- src/hierarchy/hierarchy.js
+++ src/hierarchy/hierarchy.js
@@ -4,12 +4,14 @@ d3.chart("hierarchy", {
   initialize: function() {
     var chart = this;
 
-    chart.d3      = {};
-    chart.layers  = {};
+    chart.d3     = {};
+    chart.layers = {};
 
 
     chart.base.attr("width",  chart.base.node().parentElement.clientWidth);
     chart.base.attr("height", chart.base.node().parentElement.clientHeight);
+
+    chart.d3.colorScale = chart._colors ? d3.scale.ordinal().range(chart._colors) : d3.scale.category20c();
 
     chart.d3.zoom = d3.behavior.zoom();
     chart.layers.base = chart.base.append("g");
@@ -25,8 +27,13 @@ d3.chart("hierarchy", {
     });
 
 
+    chart.on("change:colors", function() {
+      chart.d3.colorScale = d3.scale.ordinal().range(chart._colors);
+    });
+
+
     // http://bl.ocks.org/robschmuecker/7926762
-    chart.walker = function(parent, walkerFunction, childrenFunction) {
+    chart._walker = function(parent, walkerFunction, childrenFunction) {
       if( ! parent ) {
         return;
       }
@@ -36,16 +43,47 @@ d3.chart("hierarchy", {
       var children = childrenFunction(parent);
       if( children ) {
         for( var count = children.length, i = 0; i < count; i++ ) {
-          chart.walker( children[i], walkerFunction, childrenFunction );
+          chart._walker( children[i], walkerFunction, childrenFunction );
         }
       }
+    };
+
+
+    /**
+     * Initializes node attributes.
+     *
+     * @param node SVG element that represents node.
+     * @private
+     */
+    chart._initNode= function(node) {
+      node
+        .classed("leaf", function(d) { return d.isLeaf; })
+        .classed("non-leaf", function(d) { return ! d.isLeaf; });
     };
   },
 
 
 
-  transform: function(root) {
-    return root;
+  transform: function(nodes) {
+    // Before we proceed, mark leaf nodes on tree
+    this._walker(
+
+      this.root,
+      
+      function(d) { d.isLeaf = ! d.children && ! d._children; },
+      
+      function(d) {
+        if( d.children && d.children.length > 0 ) {
+          return d.children;
+        } else if( d._children && d._children.length > 0 ) {
+          return d._children;
+        } else {
+          return null;
+        }
+      }
+    );
+
+    return nodes;
   },
 
 
@@ -78,6 +116,22 @@ d3.chart("hierarchy", {
     }
 
     return this;
+  },
+
+
+  colors: function(_) {
+    if( ! arguments.length ) {
+      return this._colors;
+    }
+
+    this._colors = _;
+
+    this.trigger("change:colors");
+    if( this.root ) {
+      this.draw(this.root);
+    }
+
+    return this;    
   },
 
 
